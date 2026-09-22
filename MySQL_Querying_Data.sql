3425a11f5bf1d7c614fcfe47bf20e97e6663b053
ANEENA KM <aneenakm115@gmail.com>
2026-09-22 16:01:19 +0530

    Add files via upload

---
+++ MySQL_Querying_Data.sql
@@ -0,0 +1,156 @@
+--  DDL Commands 
+-- 1. Table Creation (CREATE):
+CREATE DATABASE Employee_database;
+USE Employee_database;
+CREATE TABLE  Departments
+	(Department_id INT PRIMARY KEY , Department_name VARCHAR(100) NOT NULL UNIQUE);
+CREATE TABLE Location	
+	(Location_id INT PRIMARY KEY AUTO_INCREMENT, Location VARCHAR(30) NOT NULL UNIQUE);
+CREATE TABLE  Employees
+	(Employee_id INT PRIMARY KEY, Employee_name VARCHAR(50) NOT NULL, Gender ENUM('M','F'), 
+	Age INT CHECK(Age >=18), Hire_date DATE DEFAULT (CURRENT_DATE), Designation VARCHAR(100), Department_id INT, Location_id INT, Salary DECIMAL(10,2),
+	FOREIGN KEY (Department_id) REFERENCES Departments(Department_id),
+    FOREIGN KEY (Location_id) REFERENCES Location(Location_id));
+SHOW TABLES;
+DESC Employees;
+DESC Departments;
+DESC Location;
+SELECT * FROM Employees;
+SELECT * FROM Departments_info;
+SELECT * FROM Locations;
+/*
+-- 2.Table Alteration (ALTER)
+ALTER TABLE Employees ADD Email VARCHAR(50); -- Email Added
+ALTER TABLE Employees MODIFY Designation VARCHAR(200); -- Designation changed 
+ALTER TABLE Employees DROP Age; -- drop age
+SELECT * FROM Employees;
+SELECT * FROM Departments;
+SELECT * FROM Location;
+ALTER TABLE Employees RENAME COLUMN Hire_Date TO Date_of_Joining ; -- Renamed hiredate
+
+-- 3. Table Renaming (RENAME)
+RENAME TABLE Departments TO Departments_Info;
+RENAME TABLE Location TO Locations;
+SHOW TABLES;
+
+-- 4. Table Truncation (TRUNCATE):
+TRUNCATE TABLE Employees;
+SELECT * FROM Employees;
+
+-- 5. Database & Table Dropping (DROP)
+DROP TABLE Employees;
+SHOW TABLES;
+DROP DATABASE Employee_database;
+*/
+
+-- MySQL Assignment 2 – Querying Data
+-- Inserting data 
+use employee_database;
+INSERT INTO departments (department_id, department_name) VALUES
+(1, 'Software Development'),
+(2, 'Marketing'),
+(3, 'Data Science'),
+(4, 'Human Resources'),
+(5, 'Product Management'),
+(6, 'Content Creation'),
+(7, 'Finance'),
+(8, 'Design'),
+(9, 'Research and Development'),
+(10, 'Customer Support'),
+(11, 'Business Development'),
+(12, 'IT'),
+(13, 'Operations');
+
+INSERT INTO location (location) VALUES
+('Chennai'),
+('Bangalore'),
+('Hyderabad'),
+('Pune');
+
+INSERT INTO employees (employee_id, employee_name, gender, age, hire_date, designation, department_id, location_id, salary) VALUES
+(5001, 'Vihaan Singh', 'M', 27, '2015-01-20', 'Data Analyst', 3, 4, 60000),
+(5002, 'Reyansh Singh', 'M', 31, '2015-03-10', 'Network Engineer', 12, 1, 80000),
+(5003, 'Aaradhya Iyer', 'F', 26, '2015-05-20', 'Customer Support Executive', 10, 2, 45000),
+(5004, 'Kiara Malhotra', 'F', 29, '2015-07-05', NULL, 8, 3, 70000),
+(5005, 'Anvi Chaudhary', 'F', 25, '2015-09-11', 'Business Development Executive', 11, 1, 55000),
+(5006, 'Dhruv Shetty', 'M', 28, '2015-11-20', 'UI Developer', 8, 2, 65000),
+(5007, 'Anushka Singh', 'F', 32, '2016-01-15', 'Marketing Manager', 2, 3, 90000),
+(5008, 'Diya Jha', 'F', 27, '2016-03-05', 'Graphic Designer', 8, 4, 70000),
+(5009, 'Kiaan Desai', 'M', 30, '2016-05-20', 'Sales Executive', 11, 3, 55000),
+(5010, 'Atharv Yadav', 'M', 29, '2016-07-10', 'Systems Administrator', 12, 4, 80000),
+(5011, 'Saanvi Patel', 'F', 28, '2016-09-20', 'Marketing Analyst', 2, 1, 60000),
+(5012, 'Myra Verma', 'F', 26, '2016-11-05', 'Operations Manager', 13, 2, 95000),
+(5013, 'Arnav Rao', 'M', 33, '2017-01-20', 'Customer Success Manager', 10, 3, 75000),
+(5014, 'Vihaan Mohan', 'M', 30, '2017-03-10', 'Supply Chain Analyst', 10, 2, 60000),
+(5015, 'Ishaan Kumar', 'M', 27, '2017-05-20', 'Financial Analyst', 7, 1, 85000),
+(5016, 'Zoya Khan', 'F', 31, '2017-07-05', 'Legal Counsel', 4, 4, 100000),
+(5017, 'Kabir Nair', 'M', 28, '2017-09-11', 'IT Support Specialist', 12, 2, 80000),
+(5018, 'Ishan Mishra', 'M', 25, '2017-11-20', 'Research Scientist', 9, 3, 75000),
+(5019, 'Ishika Patel', 'F', 29, '2018-01-15', 'Talent Acquisition Specialist', 4, 4, 55000),
+(5020, 'Aarav Nair', 'M', 32, '2018-03-05', 'Software Engineer', 1, 1, 90000),
+(5021, 'Advik Kapoor', 'M', 26, '2018-05-20', 'Finance Analyst', 7, 3, 85000),
+(5022, 'Aadhya Iyengar', 'F', 28, '2018-07-10', 'HR Specialist', 4, 4, 60000),
+(5023, 'Anika Paul', 'F', 30, '2018-09-20', 'Public Relations Specialist', 2, 2, 70000),
+(5024, 'Aryan Shetty', 'M', 27, '2018-11-05', 'Product Manager', 5, 1, 95000),
+(5025, 'Avni Iyengar', 'F', 31, '2019-01-20', 'Data Scientist', 3, 4, 100000),
+(5026, 'Vivaan Singh', 'M', 29, '2019-03-10', 'Business Analyst', 3, 2, 75000),
+(5027, 'Ananya Paul', 'F', 32, '2019-05-20', 'Content Writer', 6, 3, 60000),
+(5028, 'Anaya Kapoor', 'F', 26, '2019-07-05', 'Event Coordinator', 6, 1, 60000),
+(5029, 'Arjun Kumar', 'M', 33, '2019-09-11', 'Quality Assurance Analyst', 12, 2, 80000),
+(5030, 'Sara Iyer', 'F', 28, '2019-11-20', 'Project Manager', 5, 1, 90000);
+
+-- q1. Distinct Values: a query to retrieve distinct salaries from the Employees table
+Select Distinct Salary from Employees ;
+
+-- q2. Alias (AS):Provide aliases for the "age" and "salary" columns as "Employee_Age" and "Employee_Salary", respectively.
+Select Employee_name , Age AS Employee_Age , Salary AS Employee_Salary from Employees;
+
+-- q3. Where Clause & Operators:Retrieve employees with a salary greater than ₹50000 and hired before 2016-01-01
+Select * from Employees 
+	where Salary > 50000 AND Hire_Date < '2016-01-01';
+    
+-- q4. Find the employee whose designation is missing and fill it with "Data Scientist".
+Select * from Employees 
+	where Designation IS NULL; -- to find null designation
+Update Employees SET Designation = "Data Scientist" 
+	where employee_id = 5004; -- updating to Data scientist
+SELECT * from Employees 
+	WHERE Designation = 'Data Scientist'; -- checking whether updated
+
+-- Sorting and Grouping Data:
+-- q1. ORDER BY:Find employees sorted by department ID in ascending order and salary in descending order.
+Select * from Employees ORDER BY Department_id ASC , Salary DESC ;
+
+-- q2. LIMIT:Display the first 5 employees hired in the year 2018
+Select * from Employees where Hire_date BETWEEN '2018-01-01' and '2018-12-30' ORDER BY Hire_date ASC LIMIT 5;
+
+-- q3. Aggregate Functions: Calculate the sum of all salaries in the Finance department.
+Select SUM(Salary) AS Total_Finance_Salary from employees where department_id = 7;
+-- Find the minimum age among all employees.
+Select MIN(Age) AS  Minimum_Age from employees;
+
+-- q4. GROUP BY:List the maximum salary for each location
+Select MAX(Salary) AS Maximum_Salary , Location_id from employees group by Location_id;
+-- Calculate the average salary for each designation containing the word 'Analyst'
+Select AVG(SAlary) AS Average_Salary ,designation from employees 
+	where designation LIKE '%Analyst%' Group by Designation;
+
+-- q5. HAVING:Find departments with less than 3 employees.
+Select department_id ,Count(department_id) as count_of_department from employees 
+	group by department_id having count(department_id) < 3;
+-- Find locations with female employees whose average age is below 30.
+Select AVG(Age) as Average_age ,Location_id from Employees 
+	where Gender ='F' group by location_id having Avg(age) <30 ;
+
+-- Joins: 
+-- q1. Inner Join:List employee names, their designations, and department names where employees are assigned to a department.
+Select e.Employee_name, e.Designation,d.Department_name from Employees e 
+	INNER JOIN Departments d ON e.Department_id = d.Department_id;
+
+-- q2. Left Join:List all departments along with the total number of employees in each department, including departments with no employees.
+Select d.department_name , count(employee_id) as Employee_count from departments d 
+	left join employees e on d.Department_id = e.Department_id GROUP BY d.Department_id ,d.Department_name;
+
+-- q3. Right Join:Display all locations along with the names of employees assigned to each location. If no employees are assigned to a location, display NULL for employee name.
+SELECT e.Employee_name ,l.Location FROM Employees e
+	RIGHT JOIN Location l ON e.Location_id = l.Location_id;
